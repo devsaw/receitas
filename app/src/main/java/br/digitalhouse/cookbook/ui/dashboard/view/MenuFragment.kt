8e4a7926c7d7385e03271182ee2cf55244cf6e8e
Saulo Carvalho <saulocarvalho93@gmail.com>
2023-01-18 14:59:37 -0300

    Implementa botão remover acc

--- app/src/main/java/br/digitalhouse/cookbook/ui/dashboard/view/MenuFragment.kt
+++ app/src/main/java/br/digitalhouse/cookbook/ui/dashboard/view/MenuFragment.kt
@@ -7,16 +7,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import br.digitalhouse.cookbook.R
 import br.digitalhouse.cookbook.databinding.FragmentMenuBinding
+import br.digitalhouse.cookbook.databinding.LayoutBottomSheetAccBinding
 import br.digitalhouse.cookbook.databinding.LayoutBottomSheetBinding
 import br.digitalhouse.cookbook.ui.signin.view.SignInActivity
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 
 class MenuFragment : Fragment(R.layout.fragment_menu){
     private val binding: FragmentMenuBinding by lazy { FragmentMenuBinding.inflate(layoutInflater) }
+    private val sheetBinding: LayoutBottomSheetBinding by lazy { LayoutBottomSheetBinding.inflate(layoutInflater, null, false) }
+    private val sheetBindingAcc: LayoutBottomSheetAccBinding by lazy { LayoutBottomSheetAccBinding.inflate(layoutInflater, null, false) }
+    private var auth: FirebaseAuth? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +34,7 @@ class MenuFragment : Fragment(R.layout.fragment_menu){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        auth = FirebaseAuth.getInstance()
         setOnClickListener()
     }
 
@@ -44,6 +51,10 @@ class MenuFragment : Fragment(R.layout.fragment_menu){
 
         }
 
+        binding.desativarConta.setOnClickListener{
+            functionRemoveAcc()
+        }
+
         binding.sair.setOnClickListener{
             functionExit()
         }
@@ -54,8 +65,7 @@ class MenuFragment : Fragment(R.layout.fragment_menu){
             window!!.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
         }
 
-        val sheetBinding: LayoutBottomSheetBinding =
-            LayoutBottomSheetBinding.inflate(layoutInflater, null, false)
+       //val sheetBinding: LayoutBottomSheetBinding = LayoutBottomSheetBinding.inflate(layoutInflater, null, false)
 
         sheetBinding.btnSair.setOnClickListener{
             FirebaseAuth.getInstance().signOut()
@@ -64,6 +74,39 @@ class MenuFragment : Fragment(R.layout.fragment_menu){
         }
 
         dialog.setContentView(sheetBinding.root)
+        dialog.show()
+    }
+
+    private fun functionRemoveAcc() {
+        val dialog = BottomSheetDialog(requireContext(), R.style.BottonSheetDialog).apply {
+            window!!.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+        }
+
+       //val sheetBindingAcc: LayoutBottomSheetAccBinding = LayoutBottomSheetAccBinding.inflate(layoutInflater, null, false)
+
+        sheetBindingAcc.btnRemoveaAcc.setOnClickListener{
+            if (sheetBindingAcc.passEditText.text.toString().isNullOrEmpty()){
+                Toast.makeText(requireContext(), "Digite a senha!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val user = auth!!.currentUser
+            val credential = EmailAuthProvider.getCredential(user!!.email!!, sheetBindingAcc.passEditText.text.toString())
+            if (user != null && user.email != null) {
+                user.reauthenticate(credential).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        user.delete()
+                        Toast.makeText(requireContext(), "Sua conta foi excluída! Esperamos te ver novamente em breve!", Toast.LENGTH_LONG).show()
+                        ActivityCompat.finishAffinity(requireActivity())
+                    } else {
+                        Toast.makeText(requireContext(), "Senha incorreta!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(requireContext(), "Não é possível no momento!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        dialog.setContentView(sheetBindingAcc.root)
         dialog.show()
     }
 }
