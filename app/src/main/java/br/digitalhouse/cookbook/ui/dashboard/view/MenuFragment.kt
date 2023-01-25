b85fc0f4854cf2acde81c3bb0ffbe9cf18a7523f
Saulo Carvalho <saulocarvalho93@gmail.com>
2023-01-25 16:06:47 -0300

    Implementa alert dialog cam e galeria

--- app/src/main/java/br/digitalhouse/cookbook/ui/dashboard/view/MenuFragment.kt
+++ app/src/main/java/br/digitalhouse/cookbook/ui/dashboard/view/MenuFragment.kt
@@ -1,25 +1,39 @@
 package br.digitalhouse.cookbook.ui.dashboard.view
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import br.digitalhouse.cookbook.R
+import br.digitalhouse.cookbook.data.utils.Base64Custom
+import br.digitalhouse.cookbook.data.utils.ConfigFirebase
 import br.digitalhouse.cookbook.databinding.FragmentMenuBinding
 import br.digitalhouse.cookbook.databinding.LayoutBottomSheetAccBinding
 import br.digitalhouse.cookbook.databinding.LayoutBottomSheetBinding
 import br.digitalhouse.cookbook.databinding.LayoutBottomSheetPassBinding
+import br.digitalhouse.cookbook.ui.signin.model.User
 import br.digitalhouse.cookbook.ui.signin.view.SignInActivity
+import com.bumptech.glide.Glide
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
 
 class MenuFragment : Fragment(R.layout.fragment_menu) {
     private val binding: FragmentMenuBinding by lazy { FragmentMenuBinding.inflate(layoutInflater) }
@@ -27,6 +41,17 @@ class MenuFragment : Fragment(R.layout.fragment_menu) {
     private val sheetBindingAcc: LayoutBottomSheetAccBinding by lazy { LayoutBottomSheetAccBinding.inflate(layoutInflater, null, false) }
     private val sheetBindingPass: LayoutBottomSheetPassBinding by lazy { LayoutBottomSheetPassBinding.inflate(layoutInflater, null, false) }
     private var auth: FirebaseAuth? = null
+    private val IMAGE_CAPTURE_CODE = 1
+    private val IMAGE_STORAGE_CODE = 2
+    private lateinit var firebaseStorage: FirebaseStorage
+    private val firebaseRef = ConfigFirebase().getFirebase()
+    private var valueEventListener: ValueEventListener? = null
+    private lateinit var alertDialog: AlertDialog
+    private val permissions = arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,6 +64,9 @@ class MenuFragment : Fragment(R.layout.fragment_menu) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         auth = FirebaseAuth.getInstance()
+        firebaseStorage = FirebaseStorage.getInstance()
+        FirebaseDatabase.getInstance().getReference("usuarios")
+        setData()
         binding.progressBar.visibility = View.GONE
         setOnClickListener()
     }
@@ -66,7 +94,68 @@ class MenuFragment : Fragment(R.layout.fragment_menu) {
     }
 
     private fun funOpenOptions() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(permissions, 10)
+            }else {
+                val intentCapture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intentCapture, IMAGE_CAPTURE_CODE)}
+        } else {
+            val build = AlertDialog.Builder(requireContext(), R.style.ThemeCustomDialog)
+            val view = layoutInflater.inflate(R.layout.alert_dialog_gallery_cam, null)
+            build.setView(view)
+            val btnCam = view.findViewById<ImageButton>(R.id.camera_ib)
+            val btnGal = view.findViewById<ImageButton>(R.id.gallery_ib)
+            val btnCancel = view.findViewById<TextView>(R.id.cancel_tv)
+            btnCancel.setOnClickListener{ alertDialog.dismiss() }
+            btnCam.setOnClickListener{
+                binding.progressBar.visibility = View.VISIBLE
+                openCam()
+                alertDialog.dismiss()}
+            btnGal.setOnClickListener{
+                binding.progressBar.visibility = View.VISIBLE
+                openGal()
+                alertDialog.dismiss()}
+            alertDialog = build.create()
+            alertDialog.show()
+        }
+    }
 
+    private fun setImageClient() {
+        try {
+            firebaseStorage.reference.child("usuarios/").child(auth!!.currentUser!!.uid)
+                .downloadUrl.addOnSuccessListener { uri ->
+                    Glide
+                        .with(this)
+                        .load(uri)
+                        .error(R.drawable.user)
+                        .into(binding.imageViewClient)
+                }
+        }catch (e: Exception){
+            Log.e("STORAGE", "USUARIO SEM LOGIN")
+        }
+    }
+
+    private fun setData(){
+        val userAuth = auth!!.currentUser
+        val idUsuario = Base64Custom.codificarBase64(userAuth!!.email)
+        val nameUser = firebaseRef!!.child("usuarios").child(idUsuario)
+        valueEventListener = nameUser.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val user = snapshot.getValue(User::class.java)
+                binding.textNameUser.text = user!!.email
+            }
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.progressBar.visibility = View.GONE
+        setImageClient()
     }
 
     private fun funChangePass(){
